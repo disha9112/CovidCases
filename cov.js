418564236a2234086b772e9652cc6e45f463b3fe
Disha Singh <78133928+disha9112@users.noreply.github.com>
2021-10-30 20:17:38 +0530

    Update cov.js

--- cov.js
+++ cov.js
@@ -1,22 +1,30 @@
 const container = document.querySelector(".container");
 let arr = [];
-const servurl = "https://api.covid19api.com/live/country/india/status/confirmed/date/2021-05-03";
 
-fetch(servurl)
-.then((data) => {
+var today = new Date();
+var dd = String(today.getDate() - 1).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var yyyy = today.getFullYear();
+
+today = yyyy + "-" + mm + "-" + dd;
+
+const fetchUrl = `https://api.covid19api.com/live/country/india/status/confirmed/date/${today}`;
+
+fetch(fetchUrl)
+  .then(data => {
     return data.text();
-})
-.then((result) => {
-arr = JSON.parse(result);
+  })
+  .then(result => {
+    arr = JSON.parse(result);
 
-arr.forEach((element) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
+    arr.forEach(element => {
+      const card = document.createElement("div");
+      card.classList.add("card");
 
-    card.innerHTML = `
+      card.innerHTML = `
     <div class="cont">
     <div class="Sta">State/UT:</div>
-    <div class="sta">${element.Province}</div>     
+    <div class="sta">${element.Province}</div>
     </div>
 
     <div class="cont">  
@@ -37,9 +45,8 @@ arr.forEach((element) => {
     <div class="cont">
     <div class="Act">Active:</div>
     <div class="act">${element.Active}</div>
-    </div>`
+    </div>`;
 
-    container.appendChild(card);
-});
-
-});
+      container.appendChild(card);
+    });
+  });
